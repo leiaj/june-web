e3cc569f4aef9af6e2588d5bf9998a4b6cf07e52
github username <github email address>
2017-08-16 14:51:27 -0400

    link to menu works shows pic of menu

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
   console.log("Hi I am loaded")
   logo()
+  navBar()
+  menuImg()
 
 })
 
 function logo(){
   $('#navbar').html(function(){
     return '<center><br><br><br><img src="images/Junebarlogo.png"></img></center>'
+  })
+}
+
+function navBar(){
+  $('#actualNavBar').html(function(){
+    return '<div class="ui fluid five item menu"><a class="item" href="menu.html">Menu</a><a class="item">Events</a><a class="item">Gallery</a><a class="item">Contact</a><a class="item">Hours</a></div>'
+  })
+}
+
+function menuImg(){
+  $('#menu').html(function(){
+    return '<br><br><center><img src="images/junemenu1.png"></img><img src="images/junemenu2.png"></img></center>'
   })
 }
