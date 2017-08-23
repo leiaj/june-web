ebee089a7cfa06ebc4b8cc2109e3a2608eed5de4
github username <github email address>
2017-08-23 13:53:20 -0400

    still cant get instagram api to do what i want but changed homepage and navbar

--- index.js
+++ index.js
@@ -3,18 +3,32 @@ $(document).ready(function() {
   logo()
   navBar()
   menuImg()
-  eventImg()
+  logoBlink()
+  homePage()
+  instaGallery()
+
 })
 
 function logo(){
   $('#navbar').html(function(){
-    return '<center><br><br><br><a href="index.html"><img src="images/Junebarlogo.png"></img></a></center>'
+    return '<center><br><br><br><a href="index.html"><img src="images/Junebarlogo.png" id="blinking_image"></img></a></center>'
   })
+}
+
+function logoBlink(){
+  var img = document.getElementById('blinking_image');
+  var interval = window.setInterval(function(){
+    if(img.style.visibility == 'hidden'){
+        img.style.visibility = 'visible';
+    }else{
+        img.style.visibility = 'hidden';
+    }
+}, 500);
 }
 
 function navBar(){
   $('#actualNavBar').html(function(){
-    return '<div class="ui fluid five item menu"><a class="item" href="menu.html">Menu</a><a class="item" href="events.html">Events</a><a class="item" href="gallery.html">Gallery</a><a class="item">Contact</a><a class="item">Hours</a></div>'
+    return '<div class="ui inverted fluid three item menu"><a class="item" href="events.html">Events</a><a class="item" href="gallery.html">Gallery</a><a class="item">Contact</a></div>'
   })
 }
 
@@ -24,12 +38,22 @@ function menuImg(){
   })
 }
 
-function eventImg(){
-  var feed = new Instafeed({
-        get: 'location',
-        locationId: '1905780242995666',
-        clientId: 'bef6401d60bf42778fb1d55644f535bf',
-        accessToken: '7345015.1677ed0.0bf47f94d28943f6a5cad5e90cba76c4'
-    });
-    feed.run();
+function homePage(){
+  $('#welcome').html(function(){
+    return '<center><img src="https://media.giphy.com/media/wFKKVuqh8T9YI/giphy.gif"></img><img src="https://media.giphy.com/media/wFKKVuqh8T9YI/giphy.gif"></img><img src="https://media.giphy.com/media/wFKKVuqh8T9YI/giphy.gif"></img><img src="https://media.giphy.com/media/wFKKVuqh8T9YI/giphy.gif"></img><img src="https://media.giphy.com/media/wFKKVuqh8T9YI/giphy.gif"></img><img src="https://media.giphy.com/media/wFKKVuqh8T9YI/giphy.gif"></img><img src="https://media.giphy.com/media/wFKKVuqh8T9YI/giphy.gif"></img><br><br><img src="https://media.giphy.com/media/GqpTq4ooNy7EQ/giphy.gif"></img><img src="https://media.giphy.com/media/GqpTq4ooNy7EQ/giphy.gif"></img><img src="https://media.giphy.com/media/GqpTq4ooNy7EQ/giphy.gif"></img><img src="https://media.giphy.com/media/GqpTq4ooNy7EQ/giphy.gif"></img><img src="https://media.giphy.com/media/GqpTq4ooNy7EQ/giphy.gif"></img><img src="https://media.giphy.com/media/GqpTq4ooNy7EQ/giphy.gif"></img><img src="https://media.giphy.com/media/GqpTq4ooNy7EQ/giphy.gif"></img><img src="http://www.sevenoaksart.co.uk/images/bud04.gif"></img><img src="http://www.sevenoaksart.co.uk/images/bud04.gif"></img><img src="http://www.sevenoaksart.co.uk/images/bud04.gif"></img><img src="http://www.sevenoaksart.co.uk/images/bud04.gif"></img><img src="http://www.sevenoaksart.co.uk/images/bud04.gif"></img><img src="http://www.sevenoaksart.co.uk/images/bud04.gif"></img><img src="http://www.sevenoaksart.co.uk/images/bud04.gif"></img><img src="http://www.sevenoaksart.co.uk/images/bud04.gif"></img><img src="http://www.sevenoaksart.co.uk/images/bud04.gif"></img><img src="http://www.sevenoaksart.co.uk/images/bud04.gif"></img><img src="http://www.sevenoaksart.co.uk/images/bud04.gif"></img><img src="http://www.sevenoaksart.co.uk/images/bud04.gif"></img></center>'
+  })
 }
+
+function instaGallery(){
+  fetch('https://api.instagram.com/v1/media/search?lat=40.70252&lng=-73.91294&access_token=7345015.bef6401.88931f18e3a34515baf786432bfd90b1', {mode: 'no-cors'})
+  .then(function(response) {
+    console.log(response);
+  }).catch(function(error) {
+    console.log('Request failed', error)
+  });
+}
+
+
+
+// function eventImg(){
+// }
